ff088142d8e6deba19aa8ead45ff72004cff2f44
yaoxuanzhi <yaoxuanzhi@outlook.com>
2021-03-24 16:11:04 +0800

    修改名字

--- index.js
+++ index.js
@@ -7,12 +7,8 @@ hexo.extend.filter.register('before_generate', function(){
     console.log("增加自定义的加密");
 });
 
-const defaultConfig = {
-  'wrong_pwd_message': '密码输入错误，请重新输入'
-};
-
 hexo.extend.filter.register('after_post_render', (data) => {
-    if(data.password2 == "" || data.password2 == undefined) {
+    if(data.password == "" || data.password == undefined) {
         return data;
     }
 
@@ -22,7 +18,7 @@ hexo.extend.filter.register('after_post_render', (data) => {
 
     // data.content就是文章正文的Html
     data.origin = data.content;
-    data.content = package_encrypted_html.encrypt_html(__dirname, data.content, data.password2);
-    log.warn(`hexo-encrypt-test: encrypting "${data.password2}" "${data.title.trim()}" "${__dirname}"`);
+    data.content = package_encrypted_html.encrypt_html(__dirname, data.content, data.password);
+    log.warn(`hexo-encrypt-test: encrypting "${data.password}" "${data.title.trim()}" "${__dirname}"`);
     return data;
 }, 1000);
