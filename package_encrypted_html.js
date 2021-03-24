2a54293fb6c5849e5036a16e811309c9fcc97ea4
yaoxuanzhi <yaoxuanzhi@outlook.com>
2021-03-24 16:05:33 +0800

    调整下标签名

--- package_encrypted_html.js
+++ package_encrypted_html.js
@@ -3,18 +3,17 @@ const fs = require('fs');
 const path = require('path');
 
 function package_encrypted_content(dir, source_content, password) {
-  var inner_encrypted_page_id = "inner-encrypted-page";
   var spec_blod_text = source_content;
-  const encoded_blod_content = to_encoded_text(dir, spec_blod_text, password, inner_encrypted_page_id);
+  const encoded_blod_content = to_encoded_text(dir, spec_blod_text, password);
   return encoded_blod_content;
 }
 
 // 加密指定文本，并塞入到验证div上，更新解密后被替换的div id
-function to_encoded_text(dir, spec_blod_text, password, inner_encrypted_page_id) {
+function to_encoded_text(dir, spec_blod_text, password) {
   var temmplate_html_path = path.join(dir, 'encrypted_template_lite.html');
   const encrypted_template_content = fs.readFileSync(temmplate_html_path, 'utf8');
   const encoded_content = base64_with_xor.encode_with_xor(spec_blod_text, password);
-  const encoded_blod_content = encrypted_template_content.replace(/base64_text/g, encoded_content).replace(/spec_replace_id/g, inner_encrypted_page_id);
+  const encoded_blod_content = encrypted_template_content.replace(/base64_text/g, encoded_content);
   return encoded_blod_content;
 }
 
